fb15b4b8a90c49ea0405060d27d2e390139f31cc
Nicholas Hawn <nickhawn@hey.com>
2021-05-26 20:37:02 -0400

    clean: clean up 404 code

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,9 @@
 import Container from "../components/container";
-import Stories from "../components/posts/posts";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
 import Head from "next/head";
 
-export default function Index({ allPosts }) {
+export default function Index() {
   return (
     <>
       <Layout>
@@ -19,37 +17,15 @@ export default function Index({ allPosts }) {
         <Container>
           <Intro />
           <div className="text-center align-middle">
-            <h1 className="mb-8 text-4xl md:text-4xl font-bold tracking-tighter leading-tight">
+            <h1 className="mb-4 text-4xl md:text-4xl font-bold tracking-tighter leading-tight">
               Not Found
             </h1>
             <hdir />
-            <p>
-              Ooops! Looks like nothing was found.
-              <br />
-              Try going back to the{" "}
-              <a href="/" className="underline">
-                homepage
-              </a>
-              .
-            </p>
+            <p>Ooops! Looks like nothing was found.</p>
+            <p>Try going back to the{" "}<a href="/" className="underline">homepage</a>.</p>
           </div>
         </Container>
       </Layout>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
-
-  return {
-    props: { allPosts },
-  };
 }
